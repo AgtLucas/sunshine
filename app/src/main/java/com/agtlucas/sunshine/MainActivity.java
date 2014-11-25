b0d78835275a41da107857b0da7bd981de6518b3
Lucas <lucas@agtlucas.com>
2014-11-25 08:43:12 -0200

    Update MainActivity.java

--- app/src/main/java/com/agtlucas/sunshine/MainActivity.java
+++ app/src/main/java/com/agtlucas/sunshine/MainActivity.java
@@ -105,6 +105,8 @@ public class MainActivity extends Activity {
                 URL url = new URL("http://api.openweathermap.org/data/2.5/forecast/daily?q=94043&mode=json&units=metric&cnt=7");
 
                 urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
 
             } catch (IOException e) {
 
